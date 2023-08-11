e076152fe2d4dda6da9eb5bbaefb6c0f3d0485eb
Elizaveta Gaidarova <gaidarova.liza@gmail.com>
2023-08-11 15:04:26 +0300

    refactor

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -3,7 +3,9 @@ import axios, { AxiosResponse } from 'axios'
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
-    headers: {}
+    headers: {
+        'API-KEY': '5fc11a34-7258-4926-8c00-915edb4f940c'
+    }
 })
 
 // api
@@ -12,7 +14,7 @@ export const todolistsAPI = {
         return instance.get<TodolistType[]>('todo-lists');
     },
     createTodolist(title: string) {
-        return instance.post<ResponseType<{ item: TodolistType }>, AxiosResponse<ResponseType<{ item: TodolistType }>>, { title: string }>('todo-lists', {title});
+        return instance.post<ResponseType<{ item: TodolistType }>, AxiosResponse<ResponseType<{ item: TodolistType }>>,{ title: string }>('todo-lists', {title});
     },
     deleteTodolist(id: string) {
         return instance.delete<ResponseType>(`todo-lists/${id}`);
@@ -32,6 +34,12 @@ export const todolistsAPI = {
     updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
         return instance.put<ResponseType<{ item: TaskType }>, AxiosResponse<ResponseType<{ item: TaskType }>>, UpdateTaskModelType>(`todo-lists/${todolistId}/tasks/${taskId}`, model);
     }
+}
+
+export enum Result_Code {
+    OK = 0,
+    ERROR = 1,
+    CAPTCHA_ERROR = 10
 }
 
 // types
