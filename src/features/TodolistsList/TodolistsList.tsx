edd1d25b097d09424bc3c37b8d7fd2f26ea1dcce
Elizaveta Gaidarova <gaidarova.liza@gmail.com>
2023-09-15 09:35:24 +0300

    added prettier/refactoring

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -1,5 +1,6 @@
 import React, {useCallback, useEffect} from 'react'
-import {useAppDispatch, useAppSelector} from '../../app/store'
+import {useDispatch, useSelector} from 'react-redux'
+import {AppRootStateType} from '../../app/store'
 import {
     addTodolistTC,
     changeTodolistFilterAC,
@@ -11,21 +12,26 @@ import {
 } from './todolists-reducer'
 import {addTaskTC, removeTaskTC, TasksStateType, updateTaskTC} from './tasks-reducer'
 import {TaskStatuses} from '../../api/todolists-api'
+import {Grid, Paper} from '@material-ui/core'
 import {AddItemForm} from '../../components/AddItemForm/AddItemForm'
 import {Todolist} from './Todolist/Todolist'
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import {Navigate} from "react-router-dom";
+import { Redirect } from 'react-router-dom'
 
-export const TodolistsList: React.FC = () => {
-    const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
-    const todolists = useAppSelector<Array<TodolistDomainType>>(state => state.todolists)
-    const tasks = useAppSelector<TasksStateType>(state => state.tasks)
+type PropsType = {
+    demo?: boolean
+}
 
-    const dispatch = useAppDispatch()
+export const TodolistsList: React.FC<PropsType> = ({demo = false}) => {
+    const todolists = useSelector<AppRootStateType, Array<TodolistDomainType>>(state => state.todolists)
+    const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+
+    const dispatch = useDispatch()
 
     useEffect(() => {
-        if (!isLoggedIn) return;
+        if (demo || !isLoggedIn) {
+            return;
+        }
         const thunk = fetchTodolistsTC()
         dispatch(thunk)
     }, [])
@@ -68,9 +74,11 @@ export const TodolistsList: React.FC = () => {
     const addTodolist = useCallback((title: string) => {
         const thunk = addTodolistTC(title)
         dispatch(thunk)
-    }, [])
+    }, [dispatch])
 
-    if (!isLoggedIn) return <Navigate to={'/login'}/>
+    if (!isLoggedIn) {
+        return <Redirect to={"/login"} />
+    }
 
     return <>
         <Grid container style={{padding: '20px'}}>
@@ -84,10 +92,7 @@ export const TodolistsList: React.FC = () => {
                     return <Grid item key={tl.id}>
                         <Paper style={{padding: '10px'}}>
                             <Todolist
-                                id={tl.id}
-                                title={tl.title}
-                                filter={tl.filter}
-                                entityStatus={tl.entityStatus}
+                                todolist={tl}
                                 tasks={allTodolistTasks}
                                 removeTask={removeTask}
                                 changeFilter={changeFilter}
@@ -96,6 +101,7 @@ export const TodolistsList: React.FC = () => {
                                 removeTodolist={removeTodolist}
                                 changeTaskTitle={changeTaskTitle}
                                 changeTodolistTitle={changeTodolistTitle}
+                                demo={demo}
                             />
                         </Paper>
                     </Grid>
